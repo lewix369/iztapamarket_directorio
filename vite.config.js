d2759de8131c0d4945aa454981227a3ea6b90e3b
lewix369 <luis.carrillo.laguna@gmail.com>
2025-05-22 00:52:08 -0600

    Ajustes finales vite.config.js y vercel.json

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'node:path';
 
 export default defineConfig({
+  base: './', // 👈 CLAVE para Vercel y dominios personalizados
   plugins: [react()],
   resolve: {
     alias: {
