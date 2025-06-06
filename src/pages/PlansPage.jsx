d6741562fd1fc865c8bd4fafd80274f3e2818e5d
lewix369 <luis.carrillo.laguna@gmail.com>
2025-06-05 20:16:04 -0600

    ✅ Se actualizó el logo en la sección de planes

--- src/pages/PlansPage.jsx
+++ src/pages/PlansPage.jsx
@@ -88,7 +88,7 @@ const officialWhatsAppNumber = "525647547221";
 
 const PlansPage = () => {
   const pageHeaderImageUrl =
-    "https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80";
+    "https://storage.googleapis.com/hostinger-horizons-assets-prod/726a303c-0921-4d09-936a-1d5c7a669090/360461f31659ff0a75c4e5891d625b0a.png";
 
   return (
     <div className="container mx-auto px-4 py-12">
