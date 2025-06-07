c2c4b7e0ac388a46252f3634decbdc10ea89761e
lewix369 <luis.carrillo.laguna@gmail.com>
2025-06-06 21:53:40 -0600

    Fix: Se reemplaza página /categorias con componente CategoriesView funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import CategoriesPage from "@/pages/categorias/index";
+import CategoriesView from "@/pages/CategoriesView";
 import CategoryBusinessesPage from "@/pages/CategoryBusinessesPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/categorias" element={<CategoriesPage />} />
+        <Route path="/categorias" element={<CategoriesView />} />
         <Route path="/categorias/:slug" element={<CategoryBusinessesPage />} />
       </Routes>
     </Router>
